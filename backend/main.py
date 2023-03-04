9ced6523bf3829e2ba1ef4e8b1d5121aba16ba9c
Ganesh-P03 <84033688+Ganesh-P03@users.noreply.github.com>
2023-03-04 16:46:25 +0530

    added cors,running tasks in terminal

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,22 @@
+from fastapi import FastAPI;
 from typing import Union;
 import requests
-from fastapi import FastAPI;
 
 app = FastAPI();
 
+#enable CORS
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    return response
+
+# test route
 @app.get("/")
-def read_root():
-    return {"Hello":"World"};
+async def root():
+    return {"message": "Hello World"}
 
 # gets data from osv database for the given package
 @app.get("/osv")
--- debt-detective/src/extension.ts
+++ debt-detective/src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 import { PkgInstaller } from "./pkgInstaller";
 import { SidebarDataProvider } from "./sidebarDataProvider";
+import fs = require("fs");
 
 async function getDiagnostics(
   doc: vscode.TextDocument
@@ -9,37 +10,74 @@ async function getDiagnostics(
   const diagnostics = new Array<vscode.Diagnostic>();
 
   const textArr: string[] = text.split(/\r\n|\n/);
-  const packages = textArr;
-  //console.log(packages);
 
-  for (let i = 0; i < packages.length; i++) {
-    const pkg = packages[i];
+  const packages: string[] = [];
 
-    diagnostics.push({
-      severity: vscode.DiagnosticSeverity.Information,
-      message: `Package ${pkg} is not installed`,
-      code: "pkg-installer",
-      source: "pkg-installer",
-      range: new vscode.Range(i, 0, pkg.length, i),
-    });
+  for (let i = 0; i < textArr.length; i++) {
+    const line = textArr[i];
+    if (line.includes("import")) {
+      const pkg = line.split(" ")[1];
+      packages.push(pkg);
+    }
   }
+
+  //trying for one package
+
+  //run pipdeptree to get the dependencies in the terminal in background
+
+  /*
+    script to run in terminal
+
+    1) venv\Scripts\activate  activate venv
+    2) pipdeptree -p <package_name> > output.txt
+  */
+
+  const shellExec = new vscode.ShellExecution(
+    `venv\\Scripts\\activate && pipdeptree -p ${packages[0]}> output.txt`
+  );
+
+  //execute the task and get the output stored in a variable named depVersions
+
+  await vscode.tasks.executeTask(
+    new vscode.Task(
+      { type: "pkginstaller" },
+      vscode.TaskScope.Workspace,
+      "PkgInstaller",
+      "Pkg Installer",
+      shellExec,
+      "pipdeptree"
+    )
+  );
+
+  // const depVersions = await getDepFromOutput();
+  // console.log(depVersions);
 
   return diagnostics;
 }
 
+async function getDepFromOutput(): Promise<string[]> {
+  const depVersions: string[] = [];
+  console.log("getDepFromOutput");
+  const text = await vscode.workspace.openTextDocument("output.txt");
+  console.log(text);
+  const textArr: string[] = text.getText().split(/\r\n|\n/);
+
+  depVersions.push(textArr[0]);
+
+  return depVersions;
+}
+
 export async function activate(context: vscode.ExtensionContext) {
   const diagnosticCollection =
-    vscode.languages.createDiagnosticCollection("pkg-installer");
+    vscode.languages.createDiagnosticCollection("dependencies");
 
   const handler = async (doc: vscode.TextDocument) => {
-    if (!doc.fileName.endsWith(".js")) {
+    if (!doc.fileName.endsWith(".py")) {
       return;
     }
-    //console.log("handler");
-    //console.log(doc.fileName);
+    console.log("handler");
     const diagnostics = await getDiagnostics(doc);
     diagnosticCollection.set(doc.uri, diagnostics);
-    // console.log(diagnostics);
   };
 
   if (vscode.window.activeTextEditor) {
@@ -51,10 +89,20 @@ export async function activate(context: vscode.ExtensionContext) {
     handler(e.document)
   );
   const didSave = vscode.workspace.onDidSaveTextDocument((doc) => handler(doc));
-  const codeActionProvider = vscode.languages.registerCodeActionsProvider(
-    "javascript",
-    new PkgInstaller(context)
-  );
+  // const codeActionProvider = vscode.languages.registerCodeActionsProvider(
+  //   "javascript",
+  //   new PkgInstaller(context)
+  // );
+
+  //show all the documents in the workspace
+
+  // const uris = await vscode.workspace.findFiles("**/*.js");
+
+  // for (const uri of uris) {
+  //   console.log(uri);
+  //   // const doc = await vscode.workspace.openTextDocument(uri);
+  //   // await handler(doc);
+  // }
 
   const rootPath =
     vscode.workspace.workspaceFolders &&
@@ -62,18 +110,18 @@ export async function activate(context: vscode.ExtensionContext) {
       ? vscode.workspace.workspaceFolders[0].uri.fsPath
       : "";
 
-  const sidebarDataProvider = new SidebarDataProvider(rootPath);
-  vscode.window.registerTreeDataProvider("debt-detective", sidebarDataProvider);
-  vscode.window.createTreeView("debt-detective", {
-    treeDataProvider: sidebarDataProvider,
-  });
+  // const sidebarDataProvider = new SidebarDataProvider(rootPath);
+  // vscode.window.registerTreeDataProvider("debt-detective", sidebarDataProvider);
+  // vscode.window.createTreeView("debt-detective", {
+  //   treeDataProvider: sidebarDataProvider,
+  // });
 
   context.subscriptions.push(
     diagnosticCollection,
     didOpen,
     didSave,
-    didChange,
-    codeActionProvider
+    didChange
+    //codeActionProvider
   );
 }
 
