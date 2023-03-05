20e2ba10535cb181ac55780d90f4d017ecbe29f8
sarg30 <girotrasarthak16@gmail.com>
2023-03-05 20:06:19 +0530

    fetching packages data from frontend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI;
+from fastapi import FastAPI, Request;
 from typing import Union;
 import requests
 
@@ -21,10 +21,18 @@ async def root():
 # gets data from osv database for the given package
 @app.get("/osv")
 async def osv():
-    
+
     url = "https://api.osv.dev/v1/query"
     data = '{"version": "0.9.6", "package": {"name": "numpy", "ecosystem": "PyPI"}}'
 
     response = requests.post(url, data=data)
     
     return response.json()
+
+@app.post("/safety")
+async def safety(packages: Request):
+    packages_info = await packages.json()
+    
+    return {
+        "data": packages_info
+    }
