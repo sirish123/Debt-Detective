61b6ba010c4f5c4cb5ecf21c64d39574c1fe4a0d
Ganesh-P03 <84033688+Ganesh-P03@users.noreply.github.com>
2023-03-08 23:37:49 +0530

    passing data from extension.ts to react panel,gettng data from backend server through api calls

--- debt-detective/ext-src/extension.ts
+++ debt-detective/ext-src/extension.ts
@@ -1,20 +1,15 @@
-import * as path from "path";
 import * as vscode from "vscode";
+import ReactPanel from "./panel";
 import axios from "axios";
-
-import {
-  WebviewViewProvider,
-  WebviewView,
-  Webview,
-  Uri,
-  EventEmitter,
-  window,
-} from "vscode";
 
 export let required: { [key: string]: string } = {};
 
+let message = "Hello World from React";
+
 async function getDepOfPkg(doc: vscode.TextDocument) {
   const text = doc.getText();
+
+  message = "updating...";
 
   const textArr: string[] = text.split(/\r\n|\n/);
 
@@ -32,6 +27,7 @@ async function getDepOfPkg(doc: vscode.TextDocument) {
     `venv\\Scripts\\activate && pipdeptree -p ${packages[0]} > output.txt`
   );
 
+  message = "updating2...";
   vscode.tasks.executeTask(
     new vscode.Task(
       { type: "pkginstaller" },
@@ -42,8 +38,32 @@ async function getDepOfPkg(doc: vscode.TextDocument) {
       "pipdeptree"
     )
   );
+}
 
-  vscode.tasks.onDidEndTask(async () => {
+export async function makeApiCall(required: { [key: string]: string }) {
+  const data = await axios.get(`http://127.00.1:8000/pkg`, {
+    params: { required },
+  });
+  console.log(data);
+  message = "done";
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const handler = (doc: vscode.TextDocument) => {
+    if (!doc.fileName.endsWith(".py")) {
+      return;
+    }
+    console.log("inside handler");
+    getDepOfPkg(doc);
+  };
+
+  if (vscode.window.activeTextEditor) {
+    handler(vscode.window.activeTextEditor.document);
+  }
+
+  const didSave = vscode.workspace.onDidSaveTextDocument((doc) => handler(doc));
+
+  const onDidEndTask = vscode.tasks.onDidEndTask(async () => {
     const uri = await vscode.workspace.findFiles("output.txt");
 
     if (uri.length > 0) {
@@ -78,174 +98,27 @@ async function getDepOfPkg(doc: vscode.TextDocument) {
         }
       }
       console.log(required);
-      makeApiCall(required);
+      const data = await axios.get(`http://127.00.1:8000/pkg`, {
+        params: { required },
+      });
+
+      message = data.data["message"];
+
+      context.subscriptions.push(
+        vscode.window.registerWebviewViewProvider(
+          "react-webview.webview",
+          new ReactPanel(
+            context.extensionUri,
+            context.extensionPath,
+            message,
+            vscode.ViewColumn.One
+          )
+        )
+      );
     } else {
       console.log("no file");
     }
   });
-}
 
-export async function makeApiCall(required: { [key: string]: string }) {
-  const data = await axios.get(`http://127.00.1:8000/pkg`, {
-    params: { required },
-  });
-  console.log(data);
-}
-
-export async function activate(context: vscode.ExtensionContext) {
-  const handler = async (doc: vscode.TextDocument) => {
-    if (!doc.fileName.endsWith(".py")) {
-      return;
-    }
-    console.log("inside handler");
-    await getDepOfPkg(doc);
-  };
-
-  if (vscode.window.activeTextEditor) {
-    await handler(vscode.window.activeTextEditor.document);
-  }
-
-  const didSave = vscode.workspace.onDidSaveTextDocument((doc) => handler(doc));
-
-  context.subscriptions.push(
-    didSave,
-    vscode.window.registerWebviewViewProvider(
-      "react-webview.webview",
-      new ReactPanel(
-        context.extensionUri,
-        context.extensionPath,
-        vscode.ViewColumn.One
-      )
-    )
-  );
-}
-
-/**
- * Manages react webview panels
- */
-class ReactPanel implements WebviewViewProvider {
-  /**
-   * Track the currently panel. Only allow a single panel to exist at a time.
-   */
-  public static currentPanel: ReactPanel | undefined;
-
-  private static readonly viewType = "react";
-
-  private readonly _panel: vscode.WebviewPanel;
-  private readonly _extensionPath: string;
-  private _extensionUri: Uri;
-  private _disposables: vscode.Disposable[] = [];
-
-  constructor(
-    private readonly extensionUri: Uri,
-    private readonly extensionPath: string,
-    private data: any,
-    private _view: any = null
-  ) {
-    this._extensionPath = extensionPath;
-    this._extensionUri = extensionUri;
-  }
-
-  private onDidChangeTreeData: EventEmitter<any | undefined | null | void> =
-    new EventEmitter<any | undefined | null | void>();
-
-  refresh(context: any): void {
-    this.onDidChangeTreeData.fire(null);
-    // this._view.webview.html = this._getHtmlForWebview(this._view?.webview);
-    this._view.webview.html = this._getHtmlForWebview();
-  }
-
-  //called when a view first becomes visible
-  resolveWebviewView(webviewView: WebviewView): void | Thenable<void> {
-    webviewView.webview.options = {
-      enableScripts: true,
-      localResourceRoots: [this._extensionUri],
-    };
-    // webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
-    webviewView.webview.html = this._getHtmlForWebview();
-    this._view = webviewView;
-    this.activateMessageListener();
-  }
-
-  private activateMessageListener() {
-    this._view.webview.onDidReceiveMessage(
-      (message: { action: any; data: { message: string } }) => {
-        switch (message.action) {
-          case "SHOW_WARNING_LOG":
-            window.showWarningMessage(message.data.message);
-            break;
-          default:
-            break;
-        }
-      }
-    );
-  }
-
-  public dispose() {
-    ReactPanel.currentPanel = undefined;
-
-    // Clean up our resources
-    this._panel.dispose();
-
-    while (this._disposables.length) {
-      const x = this._disposables.pop();
-      if (x) {
-        x.dispose();
-      }
-    }
-  }
-
-  private _getHtmlForWebview() {
-    const manifest = require(path.join(
-      this._extensionPath,
-      "build",
-      "asset-manifest.json"
-    ));
-    const mainScript = manifest["files"]["main.js"];
-    const mainStyle = manifest["files"]["main.css"];
-
-    const scriptPathOnDisk = vscode.Uri.file(
-      path.join(this._extensionPath, "build", mainScript)
-    );
-    const scriptUri = scriptPathOnDisk.with({ scheme: "vscode-resource" });
-    const stylePathOnDisk = vscode.Uri.file(
-      path.join(this._extensionPath, "build", mainStyle)
-    );
-    const styleUri = stylePathOnDisk.with({ scheme: "vscode-resource" });
-
-    // Use a nonce to whitelist which scripts can be run
-    const nonce = getNonce();
-
-    return `<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="utf-8">
-				<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-				<meta name="theme-color" content="#000000">
-				<title>React App</title>
-				<link rel="stylesheet" type="text/css" href="${styleUri}">
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}';style-src vscode-resource: 'unsafe-inline' http: https: data:;">
-				<base href="${vscode.Uri.file(path.join(this._extensionPath, "build")).with({
-          scheme: "vscode-resource",
-        })}/">
-			</head>
-
-			<body>
-				<noscript>You need to enable JavaScript to run this app.</noscript>
-				<div id="root"></div>
-				
-				<script nonce="${nonce}" src="${scriptUri}"></script>
-			</body>
-			</html>`;
-  }
-}
-
-function getNonce() {
-  let text = "";
-  const possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < 32; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
+  context.subscriptions.push(didSave, onDidEndTask);
 }
