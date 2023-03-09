9d6be6826fba5467cf65584a62e33148170b5d0d
CS20B031 PUPPALA VISHNU VARDHAN <cs20b031@iittp.ac.in>
2023-03-09 19:36:47 +0530

    Extension Frontend Done!

--- debt-detective/src/App.tsx
+++ debt-detective/src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import "./App.css";
-
 import logo from "./logo.svg";
 import Test from "./test";
 
@@ -10,21 +9,13 @@ class App extends React.Component<
   constructor(props: any) {
     super(props);
   }
-
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <br></br>
-        {this.props.parameter}
+        
+      <div className="App bg-dark">
         <Test />
       </div>
+
     );
   }
 }
--- debt-detective/src/test.tsx
+++ debt-detective/src/test.tsx
@@ -1,77 +1,81 @@
 import React, { useState } from "react";
-import { PieChart, Pie, Cell } from 'recharts';
+import Parameter from "./parameter";
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis
+} from "recharts";
 
 const Test = () => {
-  
-  const RADIAN = Math.PI / 180;
-const data = [
-  { name: 'A', value: 80, color: '#ff0000' },
-  { name: 'B', value: 45, color: '#00ff00' },
-  { name: 'C', value: 25, color: '#0000ff' },
-];
-const cx = 150;
-const cy = 200;
-const iR = 50;
-const oR = 100;
-const value = 50;
 
-const needle = (value: number, data: any[], cx: number, cy: number, iR: number, oR: number, color: any) => {
-  let total = 0;
-  data.forEach((v) => {
-    total += v.value;
-  });
-  const ang = 180.0 * (1 - value / total);
-  const length = (iR + 2 * oR) / 3;
-  const sin = Math.sin(-RADIAN * ang);
-  const cos = Math.cos(-RADIAN * ang);
-  const r = 5;
-  const x0 = cx + 5;
-  const y0 = cy + 5;
-  const xba = x0 + r * sin;
-  const yba = y0 - r * cos;
-  const xbb = x0 - r * sin;
-  const ybb = y0 + r * cos;
-  const xp = x0 + length * cos;
-  const yp = y0 + length * sin;
-  return [
-    <circle cx={x0} cy={y0} r={r} fill={color} stroke="none" />,
-    <path d={`M${xba},${yba}L${xbb},${ybb},L${xp},${yp},L${xba},${yba}`} stroke="#none" fill={color} />,
+  const data = [
+    {
+      name: "Parameter1",
+      A: 25,
+      fullMark: 100
+    },
+    {
+      name: "Parameter2",
+      A: 50,
+      fullMark: 100
+    },
+    {
+      name: "Parameter3",
+      A: 40,
+      fullMark: 100
+    },
+    {
+      name: "Parameter4",
+      A: 27,
+      fullMark: 100
+    },
+    {
+      name: "Parameter5",
+      A: 61,
+      fullMark: 100
+    }
   ];
-};
+
 
   return (
-    <div className="text-center">
+    <div className="">
       {/* <p className="bg-success">Bootstrap Testing!</p> */}
 
-      <h3 className="text-light">Your Score</h3>
+      <h3 className="text-light text-center">Your Score</h3>
 
-      <PieChart width={300} height={250}>
-        <Pie
-          dataKey="value"
-          startAngle={180}
-          endAngle={0}
+      <div className="Score_Graph d-flex align-items-center justify-content-center m-3 p-1 rounded">
+        <RadarChart 
+          cx={200}
+          cy={150}
+          outerRadius={100}
+          width={400}
+          height={270}
           data={data}
-          cx={cx}
-          cy={cy}
-          innerRadius={iR}
-          outerRadius={oR}
-          fill="#8884d8"
-          stroke="none"
         >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={entry.color} />
-          ))}
-        </Pie>
-        {needle(value, data, cx, cy, iR, oR, '#d0d000')}
-      </PieChart>
-      
-      <div className="container mt-3 text-center">
-        <h3 className="text-light">Analysis by Debt Detector</h3>
-        <div className="row bg-dark p-1">
-          <p className="col-5 text-light">Parameter-1</p>
-          <p className="col-6 bg-success w-75">75%</p>
-        </div>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="name" />
+          <PolarRadiusAxis />
+          <Radar
+            name="Score"
+            dataKey="A"
+            stroke="#ff0000"
+            fill="#8884d8"
+            fillOpacity={0.6}
+          />
+        </RadarChart>
+      </div>
+
+      <div className="container m-3 Analysis rounded">
+        <h3 className="text-light text-center my-4">Analysis by Debt Detector</h3>
         
+        <Parameter name="Parameter1" percent="50%" knowmore="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+        <Parameter name="Parameter2" percent="25%" knowmore="Lorem Ipsum" />
+        <Parameter name="Parameter3" percent="61%" knowmore="Lorem Ipsum" />
+        <Parameter name="Parameter4" percent="27%" knowmore="Lorem Ipsum" />
+        <Parameter name="Parameter5" percent="47%" knowmore="Lorem Ipsum" />
+
       </div>
     </div>
   );
