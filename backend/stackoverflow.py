e83afbebec8070d2f368e830f5c6b63da1dd3603
sarg30 <girotrasarthak16@gmail.com>
2023-03-09 22:47:27 +0530

    stackoverflow scraping working

--- backend/stackoverflow.py
+++ backend/stackoverflow.py
@@ -1,23 +1,16 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+from urllib.request import urlopen
+import csv
 
-# define the URL to scrape
-url = "https://stackoverflow.com/questions/tagged/python"
+URL = "https://stackoverflow.com/questions"
 
-# define the headers to simulate a browser visit
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+response = urlopen(URL)
+html = response.read()
 
-# send a GET request to the URL
-response = requests.get(url, headers=headers)
+soup = BeautifulSoup(html, features="html.parser")
 
-# parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+questions_list = soup.find_all("h3", class_ = "s-post-summary--content-title")
 
-# find all the questions on the page
-questions = soup.find_all("div", {"class": "question-summary"})
-
-# iterate over each question and print its title and URL
-for q in questions:
-    title = q.find("a", {"class": "question-hyperlink"}).text.strip()
-    url = q.find("a", {"class": "question-hyperlink"})["href"]
-    print(f"{title}\n{url}\n")
+for q in questions_list:
+    print(q.text)
