b58f2f0ac8fda8cdbca0c9c1f8783fa17cc5e0d7
CS20B031 PUPPALA VISHNU VARDHAN <cs20b031@iittp.ac.in>
2023-03-13 18:40:41 +0530

    Improved readability

--- debt-detective/src/Debt.tsx
+++ debt-detective/src/Debt.tsx
@@ -3,35 +3,42 @@ import SummaryGraph from "./SummaryGraph";
 import PackageRenderComponent from "./PackageRenderComponent";
 import CommunityRenderaComponent from "./CommunityRenderComponent";
 
+// The Debt component receives a single prop: `data`, an object containing data about a project's technical debt
+
 const Debt = (props: any) => {
+  // useState is used to track which category the user has clicked on
   const [curSelect, setCurSelect] = useState(0);
+  // This function determines which color (danger, warning, success) to use for an indicator based on the score in `props.data`
   function getColor(i : any){
     return props.data.scores[i] >= 70 ? "danger" : (props.data.scores[i] >= 40 ? "warning" : "success");
   }
+  // These variables are used to determine the color of each category's indicator based on the score in `props.data`
   const pkg_vul_status = getColor(0);
   const activeness_status = getColor(1);
   const coding_status = getColor(2);
   const code_vul_status = getColor(3);
   const dead_code_status = getColor(4);
 
+  // This function is called when a user clicks on a category indicator
   function handleClick(x : any){
     if(x === curSelect){
-      setCurSelect(0);
+      setCurSelect(0); // if the user clicks on the already selected category, it gets deselected
     }else{
-      setCurSelect(x);
+      setCurSelect(x); // otherwise, the clicked category becomes the new selected category
     }
   }
 
+  // This function renders the appropriate content based on the selected category
   const renderBody = () => {
     if (curSelect === 0) {
-      return <></>;
+      return <></>; // if no category is selected, return nothing
     } else if (curSelect === 1) {
       return (
-        <PackageRenderComponent data={props.data} />
+        <PackageRenderComponent data={props.data} /> // if the user selects category 1, render the PackageRenderComponent with the "data" prop
       );
     } else if (curSelect === 2) {
       return (
-        <CommunityRenderaComponent data={props.data} />
+        <CommunityRenderaComponent data={props.data} /> // if the user selects category 2, render the CommunityRenderaComponent with the "data" prop
       );
     }
   };
@@ -41,10 +48,12 @@ const Debt = (props: any) => {
 
       <h2 className="text-light mb-2">Code Entropy Graph</h2>
 
+      {/* Using the component that plots the radar chart which gives insights about the scores of all parameters */}
       <SummaryGraph data={props.data} />
 
       <div className="text-light">
 
+        {/* The following five lines of code render a category indicator for each of the five categories */}
         <h5 className="text-start m-3">
           <span className={"indicator btn btn-" + pkg_vul_status} onClick={() => {handleClick(1)}}></span>
           Package Vulnerability and Security
@@ -66,6 +75,7 @@ const Debt = (props: any) => {
           Depreciated, Dead and Outdated code
         </h5>
 
+        {/* Calling the function written above to render data based on the category selected by user */}
         {renderBody()}
       </div>
     </div>
